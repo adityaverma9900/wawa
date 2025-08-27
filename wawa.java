c47e8077fe893a79333eb1fafb68b661be4be08d
adityaverma9900 <aditya.verma9900@gmail.com>
2025-08-27 20:21:57 +0530

    234f7

--- wawa.java
+++ wawa.java
@@ -1,5 +1,5 @@
 public class PrintHi {
     public static void main(String[] args) {
-        System.out.println("h fsd seconf 2345678afsdi ww wawa");
+        System.out.println("h fsd sec657657onf 2345678afsdi ww wawa");
     }
 }
